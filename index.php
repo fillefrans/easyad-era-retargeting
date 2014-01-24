4d681c5ba77b0a80e55414ac40e6ba13bb49479a
Johan Telstad <jt@enfield.com>
2014-01-24 21:16:30 +0100

    Commit before switch from CodeMirror to ACE

--- index.php
+++ index.php
@@ -1,133 +1,185 @@
 <html lang="no">
-	<head>
-		<meta charset="utf-8">
+  <head>
+    <meta charset="utf-8">
 
-		<style type="text/css">
-			.horizontal
-			{
-				width 	: 100%;
-				margin 	: 12px;
-			}
+    <style type="text/css">
 
-			.horizontal ul
-			{
-				width: 100%;
-			}
+      .sourcecode {
+        width   : 80%;
+        display : none;
+      }
 
-			.horizontal li
-			{
-				display 				: inline;
-				list-style-type 	: none;
-				padding-right 		: 20px;
-			}
-		</style>
+      .horizontal {
+        width   : 100%;
+        margin  : 12px;
+      }
 
-	</head>
-	<body>
+      .horizontal ul {
+        width : 100%;
+      }
 
+      .horizontal li {
+        display           : inline;
+        list-style-type   : none;
+        padding-right     : 20px;
+      }
 
+    </style>
 
-		<div class="horizontal">
-			<ul id="toc" style="display:inline; list-style-type: none; padding-right: 20px;"></ul>
-		</div>
+  <script src="assets/js/lib/codemirror.min.js"></script>
+  <link rel="stylesheet" href="assets/css/codemirror.css" />
 
-		<script>
-
-
-			window.addEventListener('load', function() {
-
-
-				/*  support functions  */
-
-				function insertAfter(referenceNode, newNode) {
-				    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-				}
-
-
-				function init ()
-				{
-					var 
-						i, li, newAnchor, ad,
-						toc = document.getElementById("toc"),
-						ads = document.getElementsByClassName("EAad");
-
-
-					for (var i = ads.length-1; i >= 0; i--) {
-						console.log("ad element : " + i);
-						if (null !== (ad = ads.item(i)) ) {
-							ad.addEventListener("mouseover", function(e) {
-								// console.log("ad element: ", this);
-							});
-						}
-					}
-
-
-					for (i = 0; i < document.anchors.length; i++) {
-						// skip the source anchors
-						if (document.anchors[i].name.indexOf("-src") > -1) {
-							continue;
-						}
-
-						li = document.createElement("li");
-						newAnchor = document.createElement('a');
-						newAnchor.href = "#" + document.anchors[i].name;
-						newAnchor.innerHTML = document.anchors[i].text;
-						li.appendChild(newAnchor);
-						toc.appendChild(li);
-					}
-
-				} // init ()
+  </head>
+  <body>
 
 
 
-				/*  Initialisation code  */
+    <div class="horizontal">
+      <ul id="toc" style="display:inline; list-style-type: none; padding-right: 20px;"></ul>
+    </div>
 
-				init();
-
-			}); // onload event listener
-
-		</script>
-
-	<?php
+    <script>
 
 
-	require_once('template.class.php');
 
-	$templates = array();
+      window.addEventListener('load', function() {
 
-
-	// create a list of all "[nn]x[nn].html" files in current directory
-	if ($handle = opendir('.')) {
-		while (false !== ($entry = readdir($handle))) {
-			if( (strpos($entry, '.html') !== false) && filesize($entry) ) {
-				$templates[preg_replace("/.html/", "", $entry)] = new Template($entry);
-			}
-		}
-		closedir($handle);
-	}
+        var
+          sourceEditor = null,
+          editorOptions = {
+            lineNumbers : true,
+            lineWrapping : true,
+            viewPortMargin : Infinity,
+            tabSize : 3,
+            readOnly : true
+          };
 
 
-	// set some example data for previewing purposes
-	$defaults = array(
-		"custom4" 		=> "images/test.jpg", 
-		"custom9" 		=> "Smakfull enplansvilla mot natur och strövområde", 
-		"shouttitle" 	=> "UPPSALA – LUTHAGEN, GÖTGATAN 13",
-		"shouttext" 	=> "Nyrenoverad enplansvilla om 74 välplanerade kvadrat! Stor södervänd tomt med bl a skog och allmänning som granne"
-		);
+        /*  support functions  */
+        function insertAfter(referenceNode, newNode) {
+            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+        }
 
 
-	ksort($templates);
+        function onHashChange () {
+          var
+            currentId       = window.location.hash.substr(1),
+            currentElement  = document.getElementById(currentId);
 
-	// then render all the templates with example data
-	foreach ($templates as $key => $template) {
-		print("<a name='$key' href='#$key'>$key</a><br />\n");
+          console.log("hashchange     : " + window.location.hash);
+          console.log("currentId      : " + currentId);
 
-		$template->render($defaults);
-		print("<a name='$key-src' href='#$key-src'>$key source code</a>\n");
-		$template->showSource();
-	}
+          // console.log("currentElement : " + currentElement.tagName);
+
+          if(currentElement  && ( currentElement.tagName && currentElement.tagName === "TEXTAREA")) {
+            // currentElement.style.display = "block";
+            sourceEditor = CodeMirror.fromTextArea(currentElement, editorOptions);
+            sourceEditor.getWrapperElement().style.display = 'block';
+
+            sourceEditor.on("change", function() {
+              var
+                wrap = sourceEditor.getWrapperElement(),
+                approp = sourceEditor.getScrollInfo().height > 200 ? "200px" : "auto";
+
+              if (wrap.style.height != approp) {
+                wrap.style.height = approp;
+                sourceEditor.refresh();
+              }
+            });
+
+          }
+
+        } // onHashChange
 
 
-	?>
-	</body>
+        function init () {
+          var
+            i, li, newAnchor, ad,
+            toc = document.getElementById("toc"),
+            ads = document.getElementsByClassName("EAad");
+
+
+          if ("onhashchange" in window) {
+            window.addEventListener("hashchange", onHashChange);
+          }
+
+
+          for (var i = ads.length-1; i >= 0; i--) {
+            if (null !== (ad = ads.item(i)) ) {
+              ad.addEventListener("mouseover", function(e) {
+                // console.log("ad element: ", this);
+
+              });
+            }
+          }
+
+
+          for (i = 0; i < document.anchors.length; i++) {
+
+            // skip the source anchors
+            if (document.anchors[i].name.indexOf("source") > -1) {
+              continue;
+            }
+
+            // create nav link to each ad
+            li = document.createElement("li");
+            newAnchor = document.createElement('a');
+            newAnchor.href = "#" + document.anchors[i].name;
+            newAnchor.innerHTML = document.anchors[i].text;
+            li.appendChild(newAnchor);
+            toc.appendChild(li);
+          }
+
+        } // init ()
+
+
+
+        /*  Initialisation code  */
+
+        init();
+
+      }); // onload event listener
+
+    </script>
+
+    <?php
+
+      require_once('template.class.php');
+
+      $templates = array();
+
+
+      // create a list of all "[nn]x[nn].html" files in current directory
+      if ($handle = opendir('.')) {
+        while (false !== ($entry = readdir($handle))) {
+          if( (strpos($entry, '.html') !== false) && filesize($entry) ) {
+            $templates[preg_replace("/.html/", "", $entry)] = new Template($entry);
+          }
+        }
+        closedir($handle);
+      }
+
+
+      // set some example data for previewing purposes
+      $defaults = array(
+        "custom4"     => "images/test.jpg", 
+        "custom9"     => "Smakfull enplansvilla mot natur och strövområde", 
+        "shouttitle"  => "UPPSALA – LUTHAGEN, GÖTGATAN 13",
+        "shouttext"   => "Nyrenoverad enplansvilla om 74 välplanerade kvadrat! Stor södervänd tomt med bl a skog och allmänning som granne"
+        );
+
+
+      ksort($templates);
+
+      // then render all the templates with example data
+      foreach ($templates as $key => $template) {
+        print("<p /><a name='$key' href='#$key'>$key</a>\n");
+
+        $template->render($defaults);
+        $template->showSource();
+      }
+
+    ?>
+
+  </body>
 </html>
--- template.class.php
+++ template.class.php
@@ -4,131 +4,138 @@
 class Template 
 {
 
-	private 	$filename 	= "";
+  private   $filename   = "";
+  private   $basename  = "";
 
-	// the raw template string
-	private 	$raw 			= "";
-	private 	$rendered 	= "";
-	private 	$data 		= array();
+  // the raw template string
+  private   $raw      = "";
+  private   $rendered = "";
+  private   $data     = array();
 
-
-
-	public function __construct ($filename) 
-	{
-
-		if(!file_exists($filename)) {
-			return;
-		}
-
-		$this->filename = $filename;
-
-		$this->raw = file_get_contents($filename);
-
-		$this->init();
-
-	}
-
-
-	private function init () 
-	{
-		$adSize = Template::getSizeFromFilename($this->filename);
-
-		if(count($adSize) >= 2) {
-			$this->data['adwidth'] 	= $adSize['w'];
-			$this->data['adheight'] = $adSize['h'];
-		} 
-
-		if(!isset($this->data['imagesrc'])) {
-			// default image
-			$this->data['imagesrc'] = "images/test.jpg";
-		}
-
-	}
+  private   $width    = 0;
+  private   $height   = 0;
 
 
 
+  public function __construct ($filename) 
+  {
+
+    if(!file_exists($filename)) {
+      return;
+    }
+
+    $this->filename = $filename;
+    $this->basename = basename($filename, ".html");
+
+    $this->raw = file_get_contents($filename);
+
+    $this->init();
+
+  }
 
 
-	/**
-	 *		utility functions
-	 * 
-	 */
+  private function init () 
+  {
+    $adSize = Template::getSizeFromFilename($this->filename);
 
-	private static function getSizeFromFilename ($filename) 
-	{
-		if( strpos($filename, 'x') === false ) {
-			return null;
-		}
+    $this->width  = $adSize['w'];
+    $this->height = $adSize['h'];
 
-		$size = array();
+    if(count($adSize) >= 2) {
+      $this->data['adwidth']  = $adSize['w'];
+      $this->data['adheight'] = $adSize['h'];
+    } 
 
-		$nameParts = explode("x", $filename);
+    if(!isset($this->data['imagesrc'])) {
+      // default image
+      $this->data['imagesrc'] = "images/test.jpg";
+    }
 
-		$size['w'] = intval($nameParts[0], 10);
-		$size['h'] = intval($nameParts[1], 10);
-
-		return $size;
-	}
-
-
-	private static function toRegex ($keyArray)
-	{
-
-		if(!is_array($keyArray)) {
-			return null;
-		}
-
-		$i = count($keyArray)-1;
-
-		while($i >= 0) {
-			// regex to match {key*}
-			$keyArray[$i] = "/\{(" . $keyArray[$i] . ")([^}]*)\}/";
-	 		$i--;
- 		}
- 		return $keyArray;
-	}
+  }
 
 
 
-	public function render ($contents = null, $toString = false)
-	{
-
-		$this->rendered = "";
-
-		if($contents && is_array($contents)) {
-			foreach ($contents as $key => $value) {
-				$this->data[$key] = $value;
-			}
-		}
 
 
-		$keys 	= Template::toRegex(array_keys($this->data));
-		$values 	= array_values($this->data);
+  /**
+   *    utility functions
+   * 
+   */
 
-		$this->rendered = preg_replace($keys, $values, $this->raw);
+  private static function getSizeFromFilename ($filename) 
+  {
+    if( strpos($filename, 'x') === false ) {
+      return null;
+    }
 
-		if($toString === true) {
-			return $this->rendered;
-		}
-		else {
-			print($this->rendered);
-		}
+    $size = array();
 
-	}
+    $nameParts = explode("x", $filename);
+
+    $size['w'] = intval($nameParts[0], 10);
+    $size['h'] = intval($nameParts[1], 10);
+
+    return $size;
+  }
 
 
-	public function showSource ($toString = false)
-	{
+  private static function toRegex ($keyArray)
+  {
 
-		if($toString === true) {
-			return $this->raw;
-		}
-		else {
-			print('<div style="background-color: #f8f7f7"><pre><code>' . htmlspecialchars ($this->raw) . '</code></pre></div>');
-		}
+    if(!is_array($keyArray)) {
+      return null;
+    }
 
-	}
+    $i = count($keyArray)-1;
 
+    while($i >= 0) {
+      // regex to match {key*}
+      $keyArray[$i] = "/\{(" . $keyArray[$i] . ")([^}]*)\}/";
+      $i--;
+    }
+    return $keyArray;
+  }
+
+
+
+  public function render ($contents = null, $toString = false)
+  {
+
+    $this->rendered = "";
+
+    if($contents && is_array($contents)) {
+      foreach ($contents as $key => $value) {
+        $this->data[$key] = $value;
+      }
+    }
+
+
+    $keys   = Template::toRegex(array_keys($this->data));
+    $values = array_values($this->data);
+
+    $this->rendered = preg_replace($keys, $values, $this->raw);
+
+    if($toString === true) {
+      return $this->rendered;
+    }
+    else {
+      print($this->rendered);
+    }
+
+  }
+
+
+  public function showSource ($toString = false) {
+
+    if($toString === true) {
+      return $this->raw;
+    }
+    else {
+      print(
+            "<div style='width:{$this->width}px;text-align:center;'><a name='#{$this->basename}-source' href='#{$this->basename}-source'>source</a>" . 
+            "</div>" . '<textarea id="'. $this->basename .'-source" class="sourcecode">' . $this->raw . '</textarea>');
+    }
+  }
 }
 
 
